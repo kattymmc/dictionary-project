d5305a8eef1c6b34d168f1331eec27a466e8859e
Katherine <katherine@latamready.com>
2022-02-13 23:56:33 -0500

    Search engine added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import logo from "./logo.png";
+import Dictionary from "./Dictionary"
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo"/>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo"/>
+        </header>
+      </div>
+      <main><Dictionary /></main>
+      <footer className="App-footer"><small>Coded by SheCodes</small></footer>
     </div>
   );
 }
