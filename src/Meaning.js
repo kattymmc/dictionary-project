6467edbf552dbb0dd389adab20b10849fb31cf10
Katherine <katherine@latamready.com>
2022-02-20 03:06:27 -0500

    Synonyms Added

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,7 +1,8 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-
+    console.log(props.meaning.definitions)
     return (
         <div className="Meaning">
             <h3>{props.meaning.partOfSpeech}</h3>
@@ -9,9 +10,11 @@ export default function Meaning(props) {
                 return (
                     <div key={index}>
                         <p>
-                            {definition.definition}
+                            <strong>Definition:</strong> {definition.definition}
                             <br />
+                            <strong>Example:</strong>
                             <em>{definition.example}</em>
+                            <Synonyms synonyms={definition.synonyms} />
                         </p>
                     </div>
                 );
